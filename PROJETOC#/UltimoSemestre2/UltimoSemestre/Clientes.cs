a00cead82072863bea2c12169362308461555692
Sync633 <miguelsantosp633@gmail.com>
2024-09-27 22:50:49 -0300

    att

---
+++ PROJETOC#/UltimoSemestre2/UltimoSemestre/Clientes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace UltimoSemestre
+
+{
+    internal class Clientes : Conexao
+    {
+        private string nome;
+        private string sobrenome;
+        private string cpf;
+
+        public void setNome(string nome)
+
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            this.sobrenome = sobrenome;
+        }
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+        public string getCpf()
+        {
+            return this.cpf;
+        }
+
+        public void Inserir()
+        {
+            string query = "INSERT INTO clientes(nome_cliente,sobrenome_cliente,cpf_cliente) VALUES ('" + getNome() + "','" + getSobrenome() + "','" + getCpf() + "')";
+
+            if (this.AbrirConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.FecharConexao();
+            }
+        }
+        public void Excluir()
+        {
+            string query = "Delete from clientes where cpf_cliente= '" + getCpf() + "'";
+            if (this.AbrirConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.FecharConexao();
+            }
+        }
+
+
+        public void Alterar()
+        {
+            string query = "UPDATE clientes SET nome_cliente = '" + getNome() +
+                            "', sobrenome_cliente =  '" + getSobrenome() +
+                            "'where cpf_cliente = '" + getCpf() + "'";
+
+            if (this.AbrirConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.FecharConexao();
+            }
+        }
+
+
+
+        public DataTable Consultar()
+        {
+            this.AbrirConexao();
+
+            string mSQL = "Select * from cliente ";
+
+            MySqlCommand cmd = new MySqlCommand(mSQL, conectar);
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+            this.FecharConexao();
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+    }
+}
---
+++ PROJETOC#/UltimoSemestre2/UltimoSemestre/frmTelaInicial.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UltimoSemestre
+{
+    partial class frmTelaInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            arquivoToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            cadastroToolStripMenuItem = new ToolStripMenuItem();
+            funcionáriosToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            sobreToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { arquivoToolStripMenuItem, cadastroToolStripMenuItem, sobreToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            arquivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { sairToolStripMenuItem });
+            arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            arquivoToolStripMenuItem.Size = new Size(61, 20);
+            arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(180, 22);
+            sairToolStripMenuItem.Text = "Sair";
+            sairToolStripMenuItem.Click += sairToolStripMenuItem_Click;
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            cadastroToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { funcionáriosToolStripMenuItem, clientesToolStripMenuItem });
+            cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            cadastroToolStripMenuItem.Size = new Size(66, 20);
+            cadastroToolStripMenuItem.Text = "Cadastro";
+            // 
+            // funcionáriosToolStripMenuItem
+            // 
+            funcionáriosToolStripMenuItem.Name = "funcionáriosToolStripMenuItem";
+            funcionáriosToolStripMenuItem.Size = new Size(142, 22);
+            funcionáriosToolStripMenuItem.Text = "Funcionários";
+            funcionáriosToolStripMenuItem.Click += funcionáriosToolStripMenuItem_Click;
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(142, 22);
+            clientesToolStripMenuItem.Text = "Clientes";
+            clientesToolStripMenuItem.Click += clientesToolStripMenuItem_Click;
+            // 
+            // sobreToolStripMenuItem
+            // 
+            sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            sobreToolStripMenuItem.Size = new Size(49, 20);
+            sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // frmTelaInicial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmTelaInicial";
+            Text = "frmTelaInicial";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem arquivoToolStripMenuItem;
+        private ToolStripMenuItem cadastroToolStripMenuItem;
+        private ToolStripMenuItem funcionáriosToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
